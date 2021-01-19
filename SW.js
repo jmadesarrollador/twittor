e5f409ed41440477bfac7df84597078e7a268dc3
Jesus Martinez <32663888+jmadesarrollador@users.noreply.github.com>
2021-01-19 16:24:53 -0600

    modificaciones para ios

--- SW.js
+++ SW.js
@@ -4,7 +4,7 @@ importScripts('js/SW-Utils.js');
 
 
 
-const STATIC_CACHE    = 'static-v1';
+const STATIC_CACHE    = 'static-v2';
 const DYNAMINC_CACHE  = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
